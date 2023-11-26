0736fcff3485b0f8bcb6f9899e40d19d467af729
Andrey-Yarkov <yarkov.andrey.v@gmail.com>
2023-11-26 22:03:11 +0300

    Some changes in image loading

--- app/src/main/java/com/example/hw2/adapters/ImageListRVAdapter.kt
+++ app/src/main/java/com/example/hw2/adapters/ImageListRVAdapter.kt
@@ -1,16 +1,26 @@
 package com.example.hw2.adapters
 
+import android.content.Context
+import android.content.pm.Signature
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+//import coil.load
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.hw2.R
 import com.example.hw2.classes.ImagePlate
 
 class ImageListRVAdapter(
+    val context : Context?,
     private val imagePlates: MutableList<ImagePlate>
 ) : RecyclerView.Adapter<ImageListRVAdapter.RvViewHolder>() {
 
@@ -21,14 +31,52 @@ class ImageListRVAdapter(
         // Bind item view with object
         fun bind(imagePlate : ImagePlate) {
             if (imagePlate.url != null) {
-                Glide.with(itemView)
+                Glide.with(imageView)
                     .load(imagePlate.url)
-                    .skipMemoryCache(true)
-                    .diskCacheStrategy(DiskCacheStrategy.NONE)
-                    //.thumbnail(Glide.with(itemView).load(R.drawable.image_loading))
                     .placeholder(R.drawable.image_loading)
-                    .error(R.drawable.image_error)
+                    .error(R.drawable.image_loading)
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable,
+                            model: Any,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            return false
+                        }
+
+                    })
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .transition(withCrossFade())
                     .into(imageView);
+                /*
+                Glide.with(imageView)
+                    .load(imagePlate.url)
+                    .placeholder(R.drawable.image_loading)
+                    .timeout(3000)
+                    .error(R.drawable.image_loading)
+                    //.skipMemoryCache(true)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    //.thumbnail(Glide.with(itemView).load(R.drawable.image_loading))
+                    .transition(withCrossFade())
+                    .into(imageView);
+                 */
+                /*
+                imageView.load(imagePlate.url) {
+                    placeholder(R.drawable.image_loading)
+                    error(R.drawable.image_loading)
+                }
+                 */
             }
             else {
                 imageView.setImageResource(R.drawable.default_image)
