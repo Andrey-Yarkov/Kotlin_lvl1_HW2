9ea0bbf75e1da40f492908fa6eba29162026a512
Andrey-Yarkov <yarkov.andrey.v@gmail.com>
2023-11-27 12:36:56 +0300

    Added load statuc attribute

--- app/src/main/java/com/example/hw2/classes/ImagePlate.kt
+++ app/src/main/java/com/example/hw2/classes/ImagePlate.kt
@@ -4,4 +4,5 @@ import android.media.Image
 
 data class ImagePlate(val index : Int) {
     val url : String? = "https://loremflickr.com/320/240/kitten?random=$index"
+    var isLoaded : Boolean = false
 }
