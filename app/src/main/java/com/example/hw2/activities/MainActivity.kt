a9600fdd708d061828582f2b7350346668cedd9a
Andrey-Yarkov <yarkov.andrey.v@gmail.com>
2023-11-27 22:23:58 +0300

    Retrofit functionality implemented

--- app/src/main/java/com/example/hw2/activities/MainActivity.kt
+++ app/src/main/java/com/example/hw2/activities/MainActivity.kt
@@ -1,28 +1,30 @@
 package com.example.hw2.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.hw2.R
-import com.example.hw2.adapters.ImageListRVAdapter
-import com.example.hw2.classes.ImagePlate
 import com.example.hw2.fragments.ImageListFragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val imagesBoard : Fragment = ImageListFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val imagesBoard : Fragment = ImageListFragment()
 
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fragment_container_view, imagesBoard)
             addToBackStack(null)
             commit()
         }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        cacheDir.delete()
+        Glide.get(imagesBoard.requireContext()).clearDiskCache()
     }
 }
